35c096ce1c06ec4b44d75493c6926ead37c9ab02
thechang <guschang180@hotmail.com>
2011-02-17 17:53:08 -0600

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lab4::Application.config.secret_token = '62159292435efe5de059c9d2795cd0ee5ba488386fdcf31efa178e57712c92b8ae57b24c02e4cfcc038bd3e98583cf27ab8a8917c1763d0b958636730a6fd784'
---
+++ db/migrate/20110217190917_order_id.rb~
@@ -0,0 +1,8 @@
+class OrderId < ActiveRecord::Migration
+  def self.up
+      add_column :products, :order_id, :integer
+  end
+
+  def self.down
+  end
+end
